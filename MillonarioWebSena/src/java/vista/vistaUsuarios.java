c9b353dd2395da8a7ec84ce65aebbaf124e169d0
Sthefaniabo <ladysthefania_15@hotmail.com>
2019-04-05 10:01:34 -0500

    Actualizaciones

--- MillonarioWebSena/src/java/vista/vistaUsuarios.java
+++ MillonarioWebSena/src/java/vista/vistaUsuarios.java
@@ -484,5 +484,36 @@ public class vistaUsuarios {
          FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
        
     }
+    
+     public void cambiarPass_action() {
+            try {
+                Usuario user = (Usuario) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuario");
+                String tipoUser = (String) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("tipoUsuario");
+                System.out.println("usuario: " + user.getNombreUsuario() + " tipo: " + tipoUser);
+                String claveAnterior = passClaveOld.getValue().toString();
+                String claveNueva1 = passClaveNew.getValue().toString();
+                String claveNueva2 = passClaveNew2.getValue().toString();
+                Usuario objUsuario = usuaPersistencia.find(user.getNumeroUsuario());
+                if(!objUsuario.getClaveUsuario().equals(claveAnterior)){
+                    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Error: ", "La clave anterior es incorrecta"));
+                }else if(!claveNueva1.equals(claveNueva2)){
+                    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Error: ", "Las claves no coinciden"));
+                }else{
+                    objUsuario.setClaveUsuario(claveNueva1);
+                    usuaPersistencia.edit(objUsuario);
+                    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Mensaje: ", "La Contraseña Ha Sido Actualizada con Exito"));
+                }
+            } catch (Exception ex) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error: ", ex.getMessage()));
+            }
+
+        }
+     
+      public void limpiarPass_action() {
+        passClaveNew.setValue("");
+        passClaveNew2.setValue("");
+        passClaveOld.setValue("");
+
+    }
 
 }
